5b1f855462eef90dc173c4b8bd3d2458c8c3c1ca
陈鹏 <chenpeng@chenpengdeMacBook-Pro.local>
2016-05-16 13:37:57 +0800

    123

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,15 +1,8 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var header = require('gulp-header');
-var connect = require("gulp-connect");
-var less = require("gulp-less");
-var autoprefixer = require('gulp-autoprefixer');
-var ejs = require("gulp-ejs");
-var uglify = require('gulp-uglify');
-var ext_replace = require('gulp-ext-replace');
-var cssmin = require('gulp-cssmin');
-
-var pkg = require("./package.json");
+'use strict';
+var gulp        = require('gulp');
+var plugins     = require('gulp-load-plugins')();
+var pkg         = require("./package.json");
+var browserSync = require('browser-sync').create();
 
 var banner = 
 "/** \n\
@@ -18,102 +11,65 @@ var banner =
 * http://ccppchen.github.io/jquery-weui/\n \
 */\n";
 
-gulp.task('js', function(cb) {
-
-  count = 0;
-  var end = function(){
-    count ++;
-    if(count >= 3) cb();
-  };
-
-  gulp.src([
-    './src/js/city-data.js',
-    './src/js/city-picker.js'
-  ])
-    .pipe(concat({ path: 'city-picker.js'}))
-    .pipe(gulp.dest('./dist/js/'))
-    .on("end", end);
-
-  gulp.src([
-    './src/js/swiper.jquery.js',
-    './src/js/swiper-wrap.js',
-    './src/js/photo-browser.js'
-  ])
-    .pipe(concat({ path: 'swiper.js'}))
-    .pipe(gulp.dest('./dist/js/'))
-    .on("end", end);
-
-  gulp.src([
-    './src/js/jquery-extend.js',
-    './src/js/template7.js',
-    './src/js/device.js',
-    './src/js/picker.js',
-    './src/js/select.js',
-    './src/js/calendar.js',
-    './src/js/datetime-picker.js',
-    './src/js/popup.js',
-    './src/js/notification.js',
-    './src/js/photo-browser.js'
-  ])
-    .pipe(concat({ path: 'jquery-weui.js'}))
-    .pipe(header(banner))
-    .pipe(gulp.dest('./dist/js/'))
-    .on("end", end);
-
-  
+// 监测文件改动并自动刷新
+gulp.task('server', ['compass'], function(){
+  browserSync.init({
+       server: {
+           baseDir: "./app"
+       },
+       port: 8000
+   });
 });
 
-gulp.task('uglify', ["js"], function() {
-  return gulp.src(['./dist/js/*.js', '!./dist/js/*.min.js'])
-    .pipe(uglify({
-      preserveComments: "license"
+gulp.task('watch', function(){
+  gulp.watch("sass/**/*.scss", ['compass']);
+  gulp.watch(["app/*.html", "app/styles/**/*.css", "app/images/**/*"]).on('change', browserSync.reload);
+});
+
+// 编译sass
+gulp.task('compass', function() {
+  return gulp.src("sass/**/*.scss")
+    .pipe(plugins.compass({
+      config_file: './config.rb',
+      css: 'app/styles',
+      sass: 'sass'
     }))
-    .pipe(ext_replace('.min.js'))
-    .pipe(gulp.dest('./dist/js'));
+    .pipe(plugins.autoprefixer())
 });
 
-gulp.task('less', function () {
-  return gulp.src(['./src/less/jquery-weui.less'])
-  .pipe(less())
-  .pipe(autoprefixer())
-  .pipe(header(banner))
-  .pipe(gulp.dest('./dist/css/'));
+// js合并压缩混淆
+gulp.task('js', function(){
+  gulp.src([
+      ''
+    ])
+    .pipe(plugins.jshint())
+    .pipe(jshint.reporter())
+    .pipe(plugins.concat({ path: 'main.js'}))
+    .pipe(plugins.uglify())
+    .pipe(plugins.extReplace('.min.js'))
+    .pipe(plugins.header(banner))
+    .pipe(gulp.dest("dist"))
 });
 
-gulp.task('cssmin', ["less"], function () {
-  gulp.src(['./dist/css/*.css', '!./dist/css/*.min.css'])
-    .pipe(cssmin())
-    .pipe(header(banner))
-    .pipe(ext_replace('.min.css'))
-    .pipe(gulp.dest('./dist/css/'));
+// html压缩
+gulp.task('html', function () {
+    gulp.src('app/*.html')
+    .pipe(plugins.minifyHtml())
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('ejs', function () {
-  return gulp.src(["./demos/*.html", "!./demos/_*.html"])
-    .pipe(ejs({}))
-    .pipe(gulp.dest("./dist/demos/"));
+// image压缩
+gulp.task('images', function () {
+    gulp.src('app/images/**/*')
+    .pipe(plugins.imagemin({
+        progressive: true,
+        use: [plugins.imageminPngquant()]
+    }))
+    .pipe(gulp.dest('dist/images'));
 });
 
-gulp.task('copy', function() {
-  gulp.src(['./src/lib/**/*'])
-    .pipe(gulp.dest('./dist/lib/'));
 
-  gulp.src(['./demos/images/*.*'])
-    .pipe(gulp.dest('./dist/demos/images/'));
+gulp.task('default', ['compass', 'watch', 'server']);
+gulp.task('build', ['compass', 'js', 'html', 'images', 'server']);
 
-  gulp.src(['./demos/css/*.css'])
-    .pipe(gulp.dest('./dist/demos/css/'));
-});
 
-gulp.task('watch', function () {
-  gulp.watch('src/js/**/*.js', ['js']);
-  gulp.watch('src/less/**/*.less', ['less']);
-  gulp.watch('demos/*.html', ['ejs']);
-  gulp.watch('demos/css/*.css', ['copy']);
-});
-
-gulp.task('server', function () {
-  connect.server();
-});
-
-gulp.task("default", ['js', 'less', 'copy', 'ejs', 'server']);
