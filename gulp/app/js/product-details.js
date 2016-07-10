f9295954503144d44549dc78822b7484b7d0c115
陈鹏 <chenpeng@chenpengdeMacBook-Pro.local>
2016-07-10 18:03:06 +0800

    1

--- gulp/app/js/product-details.js
+++ gulp/app/js/product-details.js
@@ -2,10 +2,10 @@ Zepto(function($) {
     $(window).on('scroll touchmove', function() {
         var percent = $(window).scrollTop() / $('.bar-header').height();
         var d = percent > 1 ? 1 : percent;
-        $('.bar-header').css('backgroundColor', 'rgba(245,245,245, '+d+')' );
+        $('.bar-header').css('backgroundColor', 'rgba(245,245,245, ' + d + ')');
         if (d >= 1) {
-            $('.bar-header .button').css('color', '#000').removeClass('bor-radius');;
-        }else {
+            $('.bar-header .button').css('color', '#000').removeClass('bor-radius');
+        } else {
             $('.bar-header .button').css('color', '#fff').addClass('bor-radius');
         };
     });
